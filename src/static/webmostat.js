3f145b0944c25bf0cbeb17e79d20904cd5b636c6
Jeroen Versteeg <jeroen@asystance.nl>
2012-12-21 22:13:38 +0100

    Fixed javascript initialization, reencoded files Removed nasty script code, replaced it with YUI module that is configured with $page Recreated files because PyScripter wrote weird Unicode control characters

---
+++ src/static/webmostat.js
@@ -0,0 +1,30 @@
+YUI.add('webmostat', function(Y) {
+	function Webmostat(config) {
+		Webmostat.superclass.constructor.apply(this, arguments);
+	}
+	Webmostat.NAME = 'webmostat';
+	Webmostat.ATTRS = {
+		page: {
+			value: 'none beeatch',
+			writeOnce: 'initOnly'
+		}
+	};
+	
+	Y.Webmostat = Y.extend(Webmostat, Y.Base, {
+		run: function () {
+			var page = this.get('page' ),
+				initFuncName = '_init' + page.charAt(0 ).toUpperCase() + page.slice(1);
+			
+			if ( !Y.Lang.isFunction(this[initFuncName]) ) {
+				alert( 'Cannot init page' );
+			}
+			this[initFuncName]();
+		},
+		
+		_initControl: function () {},
+		
+		_initTemperature: function () {},
+		
+		_initSchedule: function () {}
+	});
+}, '0.1337', {requires: ['base', 'button', 'io', 'json-stringify']} );
