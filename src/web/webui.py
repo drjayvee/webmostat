1ae9db17cbe2a6a29549b6be8ec457281e8a0f70
Jeroen Versteeg <jversteeg@gmail.com>
2012-12-30 16:55:01 +0100

    cleaned up webui.Ajax

--- src/web/webui.py
+++ src/web/webui.py
@@ -19,23 +19,19 @@ class Ajax:
     def POST(self, operation):
         params = webapi.input()
 
-        try:
-            if hasattr(self, operation) and callable(getattr(self, operation)):     # there is a function for operation
-                args = {}
-                for k, v in params.iteritems():                                     # json-decode parameters, and
-                    args[k] = json.loads(v)
+        if hasattr(self, operation) and callable(getattr(self, operation)):     # there is a function for operation
+            args = {}
+            for k, v in params.iteritems():                                     # json-decode parameters, and
+                args[k] = json.loads(v)
 
-                getattr(self, operation)( **args )                                  # call it with unpacked params
-            else:
-                raise webapi.BadRequest()
-        except AttributeError:
+            getattr(self, operation)(**args)                                    # call it with unpacked params
+        else:
             raise webapi.BadRequest()
 
         return 'ok'
 
     #TODO: move this to Thermostat class in parent package
     def setThermostat(self, pin, active):
-        print 'setThermostat:', pin, active
         command = 'sudo /usr/bin/gpiocli.py {pin} {setting} -v -q'.format(
             pin=pin, setting='HIGH' if active else 'LOW'
         )
